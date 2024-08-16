d75cd5b93c38c2822b615470b33d930e3aa538b6
Ryan Brewer <ryanbrew317@gmail.com>
2024-08-17 01:02:50 +0800

    add objects

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
-module Main where
+module Main (main) where
 import Data.Char (ord)
 import qualified Data.Map as Map
 import Data.Foldable (foldl')
@@ -62,7 +62,15 @@ many p = Parser $ \s -> do -- Either monad, not Parser monad
 exact :: String -> Parser ()
 exact s = foldr (\c p-> char c *> p) (return ()) s $> ()
 
--- | Note that this reverses the list of characters, for performance.
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy by p = do
+  b <- p
+  bs <- many0 (by >> p)
+  return (b:bs)
+
+sepBy0 :: Parser a -> Parser b -> Parser [b]
+sepBy0 by p = oneOf [sepBy by p, return []]
+
 whitespace0 :: Parser [Char]
 whitespace0 = many0 $ oneOf [char ' ', char '\n']
 
@@ -104,23 +112,60 @@ parseLet = do
   _ <- whitespace
   LetSyntax forced ident val <$> parseTerm
 
+parseObject :: Parser Syntax
+parseObject = do
+  _ <- char '{'
+  labels <- sepBy0 (char ',') $ do
+    _ <- whitespace0
+    self <- identString
+    _ <- char '.'
+    method <- identString
+    _ <- whitespace0
+    _ <- char ':'
+    def <- parseTerm
+    _ <- whitespace0
+    return (self, method, def)
+  _ <- char '}'
+  return $ ObjectSyntax labels
+
 parseParens :: Parser Syntax
 parseParens = char '(' *> parseTerm <* char ')'
 
-parseTermNoApp :: Parser Syntax
-parseTermNoApp = do
+parseTermNoPostfix :: Parser Syntax
+parseTermNoPostfix = do
   _ <- whitespace0
-  t <- oneOf [parseParens, parseInt, parseLet, parseIdentOrLambda]
+  t <- oneOf [parseParens, parseObject, parseInt, parseLet, parseIdentOrLambda]
   _ <- whitespace0
   return t
 
+data Postfix = AppPostfix Syntax
+             | AccessPostfix String
+             | UpdatePostfix String String Syntax
+
 parseTerm :: Parser Syntax
 parseTerm = do
-  t <- parseTermNoApp
-  args <- many0 parseParens
+  t <- parseTermNoPostfix
+  args <- many0 $ oneOf [
+    fmap AppPostfix parseParens,
+    fmap AccessPostfix $ char '.' >> identString,
+    do
+      _ <- whitespace0
+      _ <- exact "<-"
+      _ <- whitespace0
+      self <- identString
+      _ <- char '.'
+      method <- identString
+      _ <- whitespace0
+      _ <- char ':'
+      UpdatePostfix self method <$> parseTerm
+    ]
   let out = case args of
         [] -> t
-        _ -> foldl' AppSyntax t args
+        _ -> foldl' (\b a-> case a of
+            AppPostfix arg -> AppSyntax b arg
+            AccessPostfix method -> AccessSyntax b method
+            UpdatePostfix self method new -> UpdateSyntax b self method new
+          ) t args
   _ <- whitespace0
   return out
 
@@ -129,25 +174,52 @@ data Syntax = LambdaSyntax String Syntax
             | AppSyntax Syntax Syntax
             | IntSyntax Int
             | LetSyntax Bool String Syntax Syntax
+            | ObjectSyntax [(String, String, Syntax)]
+            | AccessSyntax Syntax String
+            | UpdateSyntax Syntax String String Syntax
             deriving Show
 
-debruijn :: Syntax -> Either String Term
-debruijn = go 0 Map.empty
+translate :: Syntax -> Term
+translate term = (\(out,_,_)->out) $ go 0 0 Map.empty Map.empty term
   where
-    go :: Int -> Map.Map String Int -> Syntax -> Either String Term
-    go index renames t = case t of
-      LambdaSyntax param body -> Lambda <$> go (index + 1) (Map.insert param index renames) body
+    go :: Int -> Int -> Map.Map String Int -> Map.Map String Int -> Syntax -> (Term, Int, Map.Map String Int)
+    go index id_gen ids renames t = case t of
+      LambdaSyntax param body ->
+        let (body2, id_gen2, ids2) = go (index + 1) id_gen ids (Map.insert param index renames) body in
+        (Lambda body2, id_gen2, ids2)
       IdentSyntax name ->
         case Map.lookup name renames of
-          Just i -> Right $ Ident (index - i - 1)
-          Nothing -> Right $ Builtin name
-      AppSyntax foo bar -> App <$> go index renames foo <*> go index renames bar
-      IntSyntax i -> Right $ Int i
-      LetSyntax True ident val scope -> do
-        val2 <- go (index + 1) (Map.insert ident index renames) val
-        scope2 <- go index renames scope
-        return $ LetForce val2 scope2
-      LetSyntax False ident val scope -> go index renames $ AppSyntax (LambdaSyntax ident scope) val
+          Just i -> (Ident (index - i - 1), id_gen, ids)
+          Nothing -> (Builtin name, id_gen, ids)
+      AppSyntax foo bar ->
+        let (foo2, id_gen2, ids2) = go index id_gen ids renames foo in
+        let (bar2, id_gen3, ids3) = go index id_gen2 ids2 renames bar in
+        (App foo2 bar2, id_gen3, ids3)
+      IntSyntax i -> (Int i, id_gen, ids)
+      LetSyntax True ident val scope ->
+        let (val2, id_gen2, ids2) = go (index + 1) id_gen ids (Map.insert ident index renames) val in
+        let (scope2, id_gen3, ids3) = go index id_gen2 ids2 renames scope in
+        (LetForce val2 scope2, id_gen3, ids3)
+      LetSyntax False ident val scope -> go index id_gen ids renames $ AppSyntax (LambdaSyntax ident scope) val
+      ObjectSyntax labels ->
+        let (labels2, id_gen2, ids2) = foldr (\(self, method, def) (so_far, idg, is)->
+                let (def2, idg2, is2) = go (index + 1) idg is (Map.insert self index renames) def in
+                case Map.lookup method is2 of
+                  Just i -> (Map.insert i def2 so_far, idg2, is2)
+                  Nothing -> (Map.insert idg2 def2 so_far, idg2 + 1, Map.insert method idg2 is2)
+              ) (Map.empty, id_gen, ids) labels in
+        (Object labels2, id_gen2, ids2)
+      AccessSyntax object method ->
+        let (object2, id_gen2, ids2) = go index id_gen ids renames object in
+        case Map.lookup method ids2 of
+          Just i -> (Access object2 i, id_gen2, ids2)
+          Nothing -> (Access object2 id_gen2, id_gen2 + 1, Map.insert method id_gen2 ids2)
+      UpdateSyntax object self method def ->
+        let (object2, id_gen2, ids2) = go index id_gen ids renames object in
+        let (def2, id_gen3, ids3) = go (index + 1) id_gen2 ids2 (Map.insert self index renames) def in
+        case Map.lookup method ids3 of
+          Just i -> (Update object2 i def2, id_gen3, ids3)
+          Nothing -> (Update object2 id_gen3 def2, id_gen3 + 1, Map.insert method id_gen3 ids3)
 
 data Term = Lambda Term
           | Ident Int
@@ -155,17 +227,23 @@ data Term = Lambda Term
           | Int Int
           | Builtin String
           | LetForce Term Term
+          | Object (Map.Map Int Term)
+          | Access Term Int
+          | Update Term Int Term
 
 class Pretty a where
   pretty :: a -> String
 
 instance Pretty Term where
-  pretty (Lambda _body) = "function"
+  pretty (Lambda _body) = "Function"
   pretty (Ident i) = "'" ++ show i
   pretty (App foo bar) = "(" ++ pretty foo ++ ")(" ++ pretty bar ++ ")"
   pretty (Int i) = show i
   pretty (Builtin name) = '$':name
-  pretty (LetForce val scope) = "push " ++ pretty val ++ " in " ++ pretty scope
+  pretty (LetForce val scope) = "force(" ++ pretty val ++ "): " ++ pretty scope
+  pretty (Object _labels) = "Object"
+  pretty (Access term label) = pretty term ++ "." ++ show label
+  pretty (Update term label new) = pretty term ++ "." ++ show label ++ " <- " ++ pretty new
 
 newtype Env = Env [(Term, Env)]
 
@@ -211,14 +289,29 @@ normalize t = go t (Stack []) (Env []) >>= \(out, _, _) -> return out
         LetForce val scope -> do
           (normal_form, _, _) <- go val (Stack []) e
           go scope s (Env $ (normal_form, e):env)
+        Object _ ->
+          case stack of
+            [] -> return (term, s, e)
+            _ -> error "cannot call an object like a function"
+        Access ob method -> do
+          (normal_form, _, _) <- go ob (Stack []) e
+          case normal_form of
+            Object labels ->
+              case Map.lookup method labels of
+                Just def -> go def s (Env $ (normal_form, e):env)
+                Nothing -> error "unknown object label"
+            _ -> error "cannot access a non-object"
+        Update ob method def -> do
+          (normal_form, _, _) <- go ob (Stack []) e
+          case normal_form of
+            Object labels ->
+              return (Object $ Map.insert method def labels, s, e)
+            _ -> error "cannot update a non-object"
 
 main :: IO ()
 main = do
   code <- getLine
   case run parseTerm code of
     Left err -> putStrLn err
-    Right (t, "") ->
-      case debruijn t of
-        Left err -> putStrLn err
-        Right t2 -> normalize t2 $> ()
+    Right (t, "") -> normalize (translate t) $> ()
     Right (_, c:_) -> putStrLn $ "unexpected `" ++ c:"`"
